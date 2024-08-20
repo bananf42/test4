12f292a1af771126b3e5a4eb1f55272b19f6b531
bananf42 <bitcodesco@proton.me>
2024-08-20 18:36:32 +0300

    Add files via upload

--- src/components/GridFeature/GridFeature.jsx
+++ src/components/GridFeature/GridFeature.jsx
@@ -21,7 +21,7 @@ const GridFeature = () => {
         {/* top grid */}
         <div className="xl:grid xl:grid-cols-12 gap-4 xl:gap-8 flex flex-col ">
           {/* first */}
-          <div className="single-content p-4 xl:p-9 bg-[#000000] xl:col-span-4   ">
+          <div className="single-content p-4 xl:p-9 bg-[#333] xl:col-span-4   ">
             <CommonTitle color="#DB0A15">AI Features</CommonTitle>
 
             {/* img wrapper */}
@@ -45,7 +45,7 @@ const GridFeature = () => {
           {/* second */}
           <div className="xl:col-span-8 gap-4 xl:gap-8 flex flex-col">
             {/* top one */}
-            <div className="single-content  w-full px-8 xl:px-[46px] py-4 xl:py-1.5 bg-[#000000] flex flex-col xl:flex-row justify-between items-center gap-4 xl:gap-0 ">
+            <div className="single-content  w-full px-8 xl:px-[46px] py-4 xl:py-1.5 bg-[#333] flex flex-col xl:flex-row justify-between items-center gap-4 xl:gap-0 ">
               <div className="space-y-4">
                 <CommonTitle color="#DB0A15">24/7 Support</CommonTitle>
 
@@ -66,7 +66,7 @@ const GridFeature = () => {
 
             {/* lower one */}
             <div className="flex gap-4 xl:gap-8 flex-col xl:flex-row ">
-              <div className="px-8 py-5 flex-1 bg-[#000000]  single-content ">
+              <div className="px-8 py-5 flex-1 bg-[#333]  single-content ">
                 {/* image */}
                 <div className="  w-[180px] h-[135px] xl:w-[220px] xl:h-[145px] mx-auto mb-6 ">
                   <img
@@ -86,7 +86,7 @@ const GridFeature = () => {
               </div>
               <div
                 className="px-8 pt-6 
-               flex-1 bg-[#000000] single-content relative "
+               flex-1 bg-[#333] single-content relative "
               >
                 <div className="space-y-4 mb-24 xl:mb-0">
                   <CommonTitle color="#DB0A15">Smart Messaging</CommonTitle>
@@ -111,7 +111,7 @@ const GridFeature = () => {
 
         {/* bottom grid */}
         <div className="xl:grid xl:grid-cols-12 gap-4 xl:gap-8 flex flex-col">
-          <div className="xl:col-span-8 single-content pl-[46px] py-0 pt-5 pb-20 xl:py-[145px] bg-[#000000] relative ">
+          <div className="xl:col-span-8 single-content pl-[46px] py-0 pt-5 pb-20 xl:py-[145px] bg-[#333] relative ">
             <div className="space-y-4 relative z-30">
               <CommonTitle color="#DB0A15">Pay-to-View</CommonTitle>
 
@@ -128,7 +128,7 @@ const GridFeature = () => {
               />
             </div>
           </div>
-          <div className="xl:col-span-4 single-content p-5 xl:p-10 bg-[#000000] relative ">
+          <div className="xl:col-span-4 single-content p-5 xl:p-10 bg-[#333] relative ">
             <div className="space-y-4 mb-24 xl:mb-0">
               <CommonTitle color="#DB0A15">Advanced Insights</CommonTitle>
 
